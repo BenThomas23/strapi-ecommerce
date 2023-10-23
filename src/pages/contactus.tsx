3368518656a16c5e66e256591d1867a8a4ba5275
Ben Thomas <75978175+BenThomas23@users.noreply.github.com>
2023-10-24 02:47:10 +0530

    Hhehe

---
+++ src/pages/contactus.tsx
@@ -0,0 +1,15 @@
+import Contact from '@/components/Contact'
+import React from 'react'
+
+import Header from '@/components/Header'
+
+
+function contactus() {
+  return (<>
+  <Header/>
+  <Contact/>
+  </>
+  )
+}
+
+export default contactus
